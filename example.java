cd25cf0a4e63ae772fff639ee3867fc84f7e4d7e
ShadowZ896 <zacharyrprzybylski@gmail.com>
2025-01-17 10:33:16 -0500

    commit 2

--- example.java
+++ example.java
@@ -1,5 +1,8 @@
 public class example{
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int mynum = 5;
+        mynum += 1;
+        System.out.println(mynum);
     }
 }
